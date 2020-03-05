eaf5f4cf157dbe619c3e8bfcedff8b09d683af3d
Nico <nicolas.p.poblete@gmail.com>
2020-03-05 10:37:11 -0300

    Initial Commit

---
+++ latam-biometric-enrollment-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  latam-biometric-enrollment-app
+//
+//  Created by Nicolas Perez on 05-03-20.
+//  Copyright © 2020 Nicolas Perez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ latam-biometric-enrollment-appTests/latam_biometric_enrollment_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  latam_biometric_enrollment_appTests.swift
+//  latam-biometric-enrollment-appTests
+//
+//  Created by Nicolas Perez on 05-03-20.
+//  Copyright © 2020 Nicolas Perez. All rights reserved.
+//
+
+import XCTest
+@testable import latam_biometric_enrollment_app
+
+class latam_biometric_enrollment_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
